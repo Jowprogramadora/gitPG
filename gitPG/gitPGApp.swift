9e978c4948b05795087ad6fec7da31af8a3cdc2f
Joana <joanavfp@hotmail.com>
2022-10-11 15:08:42 -0300

    Initial Commit

---
+++ gitPG/gitPGApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitPGApp.swift
+//  gitPG
+//
+//  Created by Joana Lopes on 11/10/22.
+//
+
+import SwiftUI
+
+@main
+struct gitPGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
